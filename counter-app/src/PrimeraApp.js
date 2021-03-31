189c0a06e48f40ecf879881c00dca35dc105c2ec
Haron Acosta <39834578+haronacosta@users.noreply.github.com>
2021-03-31 16:48:54 -0400

    Counter app

--- counter-app/src/PrimeraApp.js
+++ counter-app/src/PrimeraApp.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function PrimeraApp() {
-  return <h1>Hola mundo</h1>;
+function PrimeraApp({ saludo }) {
+  return (
+    <>
+      <h1>{saludo}</h1>
+      <p>Mi primera aplicación</p>
+    </>
+  );
 }
 
+PrimeraApp.propTypes = {
+  saludo: PropTypes.string.isRequired,
+};
 export default PrimeraApp;
