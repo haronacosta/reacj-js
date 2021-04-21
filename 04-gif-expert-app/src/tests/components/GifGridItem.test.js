101ca3a31ff5676eac379654c360d73f5668af7f
Haron Acosta <39834578+haronacosta@users.noreply.github.com>
2021-04-20 22:36:07 -0400

    Test Gif Expert App part1

--- 04-gif-expert-app/src/tests/components/GifGridItem.test.js
+++ 04-gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -12,4 +12,28 @@ describe('Pruebas en el componente GifGridItem', () => {
   test('Prueba del componente, traer el componente correctamente', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  test('Se debe ver el title en un parrafo', () => {
+    const p = wrapper.find('p');
+
+    expect(p.text().trim()).toBe(title);
+  });
+
+  test('El url debe de verse en el src y el title en el alt', () => {
+    const img = wrapper.find('img');
+
+    const { src, alt } = img.props();
+
+    expect(src).toBe(url);
+
+    expect(alt).toBe(title);
+  });
+
+  test('Nos aseguramo que el contenedor div tenga la clase animate__fadeIn', () => {
+    const div = wrapper.find('div');
+
+    const classTest = `animate__fadeIn`;
+
+    expect(div.hasClass(classTest)).toBe(true);
+  });
 });
