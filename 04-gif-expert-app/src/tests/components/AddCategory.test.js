34cd958e51b8bd944672f0113f9b0d0f871c9434
Haron Acosta <39834578+haronacosta@users.noreply.github.com>
2021-04-21 23:26:38 -0400

    Gif expert app test

--- 04-gif-expert-app/src/tests/components/AddCategory.test.js
+++ 04-gif-expert-app/src/tests/components/AddCategory.test.js
@@ -29,4 +29,21 @@ describe('Pruebas en el componente AddCategory', () => {
 
     expect(setCategories).not.toHaveBeenCalled();
   });
+
+  test('Pruebas del input, se debe llamar al setCategories y limpiar el campo', () => {
+    // 1. Simular input change
+    // 2. Simular submit
+    // 3. setCategories se debe de haber llamado setCategories
+    // 4. El valor del input debe de estar ('')
+
+    const value = 'Valor de prueba';
+
+    wrapper.find('input').simulate('change', { target: { value } });
+
+    wrapper.find('form').simulate('submit', { preventDefault() {} });
+
+    expect(setCategories).toHaveBeenCalled();
+
+    expect(wrapper.find('p').text().trim()).toBe('');
+  });
 });
